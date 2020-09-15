91e732bed2fb4ad5f1c36a7955b789133d0f61ef
Paul Garrod <58899433+PaulGarrod@users.noreply.github.com>
2020-09-15 14:46:17 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,163 @@
+let projects = [{
+	name: 'Love Local NZ',
+	website: 'http://www.lovelocalnz.com',
+	code: 'https://github.com/PaulGarrod/loveLocalNZ',
+	hosted: 'Heroku', 
+	description: 'A web application for Kiwi\'s to list their own classifieds with no fees! Built in GOORM IDE, the application was created using Javascript, Node.JS Mongo DB, Express.JS and NPM. Industry standard RESTFUL routing and CRUD operations were implemented as well as GitHub for version control. The application was launched on and is still hosted on Heroku.',
+	description2: '', 
+	tech:
+		"<span class='iconify m-3' data-icon='logos:html-5'></span> <span class='iconify m-3' data-icon='logos:css-3'></span> <span class='iconify m-3' data-icon='logos:bootstrap'></span> <span class='iconify m-3' data-icon='logos:javascript'></span> <span class='iconify m-3' data-icon='logos:nodejs-icon'></span> <span class='iconify m-3' data-icon='logos:mongodb'></span> <span class='iconify m-3' data-icon='logos:heroku-icon'></span> <span class='iconify m-3' data-icon='logos:npm-2'></span> <span class='iconify m-3' data-icon='logos:github-icon'></span>"
+	,
+	codeLink: 'https://github.com/PaulGarrod/loveLocalNZ',
+	brief: 'Create a web application which allows users to list items/ services, (similar to Craig\'s list). Application will not participate in the transfer of funds, users are encouraged to negotiate and arrange their own sales/ payments off the app.',
+	idea: 'Give Kiwi\'s a place to sell and buy from other Kiwi\'s for free.',
+	challenges: [
+		'Utilizing the database', 'Integrating reset password feature', 'Creating admin permissions', ''
+	],
+	doItDifferently: ''
+},{
+	name: 'Tom Freer Photography',
+	website: 'https://tomfreerphotography.herokuapp.com/',
+	code: 'https://github.com/PaulGarrod/tomFreerPhotography',
+	hosted: 'Heroku',
+	description: 'A photography portfolio website for a local photographer, currently designed with a simple one page layout. But built with the further expansion of his portfolio and additional show pages in mind. Built in the GOORM IDE and hosted on Heroku. Created using Javascript, Node.JS, Express JS and NPM to allow for easy expansion of the clients work when necassary. GitHub used for version control.',
+	tech:
+		"<span class='iconify m-3' data-icon='logos:html-5'></span> <span class='iconify m-3' data-icon='logos:css-3'></span> <span class='iconify m-3' data-icon='logos:bootstrap'></span> <span class='iconify m-3' data-icon='logos:javascript'></span> <span class='iconify m-3' data-icon='logos:nodejs-icon'></span> </span> <span class='iconify m-3' data-icon='logos:heroku-icon'></span> <span class='iconify m-3' data-icon='logos:npm-2'></span> <span class='iconify m-3' data-icon='logos:github-icon'></span>"
+	,
+	codeLink: 'https://github.com/PaulGarrod/tomFreerPhotography', 
+	brief: 'Simple, single page photography portfolio. No booking functions, just contact information and example photographs.',
+	idea: '',
+	challenges: [
+		''
+	]
+},{
+	name: 'Phi Phi Island Travel Site',
+	website: 'http://www.ppislands.com',
+	code: 'https://github.com/PaulGarrod/ppislands.com',
+	hosted: 'Heroku',
+	description: 'Travel information site for Phi Phi Island, Thailand. Supplimenting a Facebook group I\'m an admin for with some friends. After living on Phi Phi for over a year, I heard tourists ask the same questions over and over. So, I created an information site with relevent information about the island, travel and activities. Built in the GOORM IDE and hosted on Heroku. Created using Javascript, Node.JS, Express JS and NPM, utilizing restful routing and GitHub for version control.',
+	tech:
+		"<span class='iconify m-3' data-icon='logos:html-5'></span> <span class='iconify m-3' data-icon='logos:css-3'></span> <span class='iconify m-3' data-icon='logos:bootstrap'></span> <span class='iconify m-3' data-icon='logos:javascript'></span> <span class='iconify m-3' data-icon='logos:nodejs-icon'></span> </span> <span class='iconify m-3' data-icon='logos:heroku-icon'></span> <span class='iconify m-3' data-icon='logos:npm-2'></span> <span class='iconify m-3' data-icon='logos:github-icon'></span>"
+	,
+	codeLink: 'https://github.com/PaulGarrod/ppIslands',
+	brief: 'Deliver travel information on Phi Phi Island to help tourists maximise their trip.',
+	idea: 'Promotion of scuba diving and none party related activities with an emphasis on environmental and clean tourism.',
+	challenges: [
+		'Creating the show pages', 'Gathering all the images', 'Aligning the layout correctly', 'Using a blue on blue color theme'
+	]
+},{
+	name: 'Simple Movie Search',
+	website: 'https://pgmoviesearch.herokuapp.com/',
+	code: 'https://github.com/PaulGarrod/simpleMovieSearch',
+	hosted: 'Heroku',
+	description: 'Utilizing a movie database API, I created a Simple Movie Search which will return any movie which includes your search term along with the movies release date. Originally created using VS Code and later migrated to GOORM IDE, hosted on Heroku.',
+	tech:
+		"<span class='iconify m-3' data-icon='logos:html-5'></span> <span class='iconify m-3' data-icon='logos:css-3'></span> <span class='iconify m-3' data-icon='logos:bootstrap'></span> <span class='iconify m-3' data-icon='logos:javascript'></span> <span class='iconify m-3' data-icon='logos:heroku-icon'></span> <span class='iconify m-3' data-icon='logos:github-icon'></span>"
+	,
+	codeLink: 'https://github.com/PaulGarrod/simpleMovieSearch', 
+	challenges: [
+		'Correctly integrating the API keys', 'Two', 'Three',
+	]
+}];
+
+// function mainProject0() {
+// 		document.getElementById('mainProject0').innerHTML=(`<div class=\'container\'><div class=\'row mt-5 mb-3\'><div class=\'col-12 text-center\'><h2>${projects[0].name}</h2></div></div><div class=\'row\'><div class=\'col-md-6 p-0\'><a href=\'${projects[0].website}\'  target=\'_blank\'><img src=\'images/portfolio/${projects[0].name}.jpg\' class=\'respImg\'></a><div class=\'col-12\'><p class=\'halfSize text-center\'>${projects[0].tech}</p></div></div><div class=\'col-md-6\'><h4>Description</h4><p>${projects[0].description}</p><div class=\'text-center\'><a href=\'${projects[0].website}\' target=\'_blank\'><button class=\'text-center btnDark btn btn-secondary m-2\'>Website</button></a><a href=\'${projects[0].code}' target=\'_blank\'><button class=\'text-center btnDark btn btn-secondary m-2\'>Code</button></a></div></div></div></div>`);
+// }
+// mainProject0();
+
+// function mainProject1() {
+// 	document.getElementById('mainProject1').innerHTML=(`<div class=\'container\'><div class=\'row mt-5 mb-3\'><div class=\'col-12 text-center\'><h2>${projects[1].name}</h2></div></div><div class=\'row\'><div class=\'col-md-6 p-0\'><a href=\'${projects[1].website}\'  target=\'_blank\'><img src=\'images/portfolio/${projects[1].name}.jpg\' class=\'respImg\'></a><div class=\'col-12\'><p class=\'halfSize text-center\'>${projects[1].tech}</p></div></div><div class=\'col-md-6\'><h4>Description</h4><p>${projects[1].description}</p><div class=\'text-center\'><a href=\'${projects[1].website}\' target=\'_blank\'><button class=\'text-center btnDark btn btn-secondary m-2\'>Website</button></a><a href=\'${projects[1].code}' target=\'_blank\'><button class=\'text-center btnDark btn btn-secondary m-2\'>Code</button></a></div></div></div></div>`);
+// }
+// mainProject1();
+
+function mainProject() {
+	for(var i = 0; i<projects.length; i++){
+		document.getElementById(`mainP${i}`).innerHTML=(`<div class=\'container\'><div class=\'row mt-5 mb-3\'><div class=\'col-12 text-center\'><h2>${projects[i].name}</h2></div></div><div class=\'row\'><div class=\'col-md-6 p-0\'><a href=\'${projects[i].website}\'  target=\'_blank\'><img src=\'images/portfolio/${projects[i].name}.jpg\' class=\'respImg\'></a><div class=\'col-12\'><p class=\'halfSize text-center\'>${projects[i].tech}</p></div></div><div class=\'col-md-6\'><h4>Description</h4><p>${projects[i].description}</p><div class=\'text-center\'><a href=\'${projects[i].website}\' target=\'_blank\'><button class=\'text-center btnDark btn btn-secondary m-2\'>Website</button></a><a href=\'${projects[i].code}' target=\'_blank\'><button class=\'text-center btnDark btn btn-secondary m-2\'>Code</button></a></div></div></div></div>`);
+	}
+}
+mainProject();
+
+let projectsNetlify = [
+	{
+	name: 'HTML Canvas',
+	website: 'https://canvaspaint.netlify.app/',
+	code: 'https://github.com/PaulGarrod/HTMLcanvas',
+	img: 'canvas',	
+	description: '',
+	tech: 
+		"<span class='iconify m-3' data-icon='logos:html-5'></span> <span class='iconify m-3' data-icon='logos:css-3'></span> <span class='iconify m-3' data-icon='logos:bootstrap'></span> <span class='iconify m-3' data-icon='logos:javascript'></span> <span class='iconify m-3' data-icon='logos:heroku-icon'></span> <span class='iconify m-3' data-icon='ant-design:github-filled'></span>"
+	,
+	mobile: true,
+}, 
+	{
+	name: 'CSS Sliders',
+	website: 'https://altercssslider.netlify.app/',
+	code: 'https://github.com/PaulGarrod/cssVarsAndJS',
+	img: 'cssVariables',
+	description: '',
+	tech:
+		"<span class='iconify m-3' data-icon='logos:html-5'></span> <span class='iconify m-3' data-icon='logos:css-3'></span> <span class='iconify m-3' data-icon='logos:bootstrap'></span> <span class='iconify m-3' data-icon='logos:javascript'></span> <span class='iconify m-3' data-icon='logos:heroku-icon'></span> <span class='iconify m-3' data-icon='ant-design:github-filled'></span>"
+	,
+	mobile: true,
+}, {
+	name: 'Calculator',
+	website: 'https://paulgarrod-calculator.netlify.app/',
+	code: 'https://github.com/PaulGarrod/calculator ',
+	img: 'calculator',
+	description: '',
+	tech:
+		"<span class='iconify m-3' data-icon='logos:html-5'></span> <span class='iconify m-3' data-icon='logos:css-3'></span> <span class='iconify m-3' data-icon='logos:bootstrap'></span> <span class='iconify m-3' data-icon='logos:javascript'></span> <span class='iconify m-3' data-icon='logos:heroku-icon'></span> <span class='iconify m-3' data-icon='ant-design:github-filled'></span>"
+	,
+	mobile: true,
+}, 
+// {
+// 	name: 'Drum Kit',
+// 	website: 'https://jsdrumkit2020.netlify.app/',
+// 	code: 'https://github.com/PaulGarrod/drumKit ',
+// 	img: 'drumKit',
+// 	description: '',
+// 	tech:
+// 		"<span class='iconify m-3' data-icon='logos:html-5'></span> <span class='iconify m-3' data-icon='logos:css-3'></span> <span class='iconify m-3' data-icon='logos:bootstrap'></span> <span class='iconify m-3' data-icon='logos:javascript'></span> <span class='iconify m-3' data-icon='logos:heroku-icon'></span> <span class='iconify m-3' data-icon='ant-design:github-filled'></span>"
+// 	,
+// 	mobile: false,
+// }, 
+{
+	name: 'Clock',
+	website: 'https://paulgarrod-clock.netlify.app/',
+	code: 'https://github.com/PaulGarrod/jsClock',
+	img: 'clock',
+	description: '',
+	tech:
+		"<span class='iconify m-3' data-icon='logos:html-5'></span> <span class='iconify m-3' data-icon='logos:css-3'></span> <span class='iconify m-3' data-icon='logos:bootstrap'></span> <span class='iconify m-3' data-icon='logos:javascript'></span> <span class='iconify m-3' data-icon='logos:heroku-icon'></span> <span class='iconify m-3' data-icon='ant-design:github-filled'></span>"
+	,
+	mobile: true,
+}, {
+	name: 'To Do List',
+	website: 'https://paulgarrod-todolist.netlify.app/',
+	code: 'https://github.com/PaulGarrod/toDoList',
+	img: 'toDo',
+	description: '',
+	tech:
+		"<span class='iconify m-3' data-icon='logos:html-5'></span> <span class='iconify m-3' data-icon='logos:css-3'></span> <span class='iconify m-3' data-icon='logos:bootstrap'></span> <span class='iconify m-3' data-icon='logos:javascript'></span> <span class='iconify m-3' data-icon='logos:heroku-icon'></span> <span class='iconify m-3' data-icon='ant-design:github-filled'></span>"
+	,
+	mobile: true,
+}, {
+	name: 'City Search',
+	website: 'https://searchapis.netlify.app/',
+	code: 'https://github.com/PaulGarrod/ajaxTypeAhead',
+	img: 'citySearch',
+	description: '',
+	tech:
+		"<span class='iconify m-3' data-icon='logos:html-5'></span> <span class='iconify m-3' data-icon='logos:css-3'></span> <span class='iconify m-3' data-icon='logos:bootstrap'></span> <span class='iconify m-3' data-icon='logos:javascript'></span> <span class='iconify m-3' data-icon='logos:heroku-icon'></span> <span class='iconify m-3' data-icon='ant-design:github-filled'></span>"
+	,
+	mobile: true,
+}];
+
+function smallProject() {
+	for(var i = 0; i<projectsNetlify.length; i++){
+		document.getElementById(`netlifyP${i}`).innerHTML=(`<h4 class=\'mt-4\'>${projectsNetlify[i].name}</h4><a href=\'${projectsNetlify[i].website}\'  target=\'_blank\'><img src=\'images/portfolio/netlify/${projectsNetlify[i].img}.JPG\' class=\'respImg\'></a><div class=\'text-center\'><a href=\'${projectsNetlify[i].website}\' target=\'_blank\'><button class=\'text-center btnLight btn btn-secondary btn-sm m-1 d-inline\'>Website</button></a><a href=\'${projectsNetlify[i].code}\' target=\'_blank\'><button class=\'text-center btnLight btn btn-secondary btn-sm m-1 d-inline\'>Code</button></a></div></div></div>`
+		)}
+	};
+smallProject();
+
+export {projects, projectsNetlify}
